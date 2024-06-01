dbf6c66257a9b038ee57f2eb2f83cfe5683a5f73
salman <salmanwaheed2410@gmail.com>
2024-06-01 17:28:32 +0500

    Completed the feed page, added new functionalities like copying text

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -5,7 +5,17 @@ import { useState, useEffect } from "react";
 import PromptCard from "./PromptCard";
 
 const PromptCardList = ({ data, handleTagClick }) => {
-  return <div className="mt-16 prompt_layout"></div>;
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
 };
 
 const Feed = () => {
@@ -18,12 +28,12 @@ const Feed = () => {
     const fetchPosts = async () => {
       const response = await fetch("/api/prompt");
       const data = await response.json();
-
       setPosts(data);
     };
 
     fetchPosts();
   }, []);
+
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
@@ -36,7 +46,7 @@ const Feed = () => {
           className="search_input peer"
         />
       </form>
-      <PromptCardList data={[]} handleTagClick={() => {}} />
+      <PromptCardList data={posts} handleTagClick={() => {}} />
     </section>
   );
 };
